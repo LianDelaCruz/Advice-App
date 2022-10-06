044f56e6cd3d5cde785aa83271a1bb98f8d97e22
Takikato09 <liandelacruz09@hotmail.com>
2022-10-06 20:50:15 +0200

    adds style
    I added more styling and tried to change the visibility of the list and clear button

--- Advice App/script.js
+++ Advice App/script.js
@@ -1,5 +1,6 @@
 const currentAdvice = document.querySelector('#current-advice-content');
 const adviceListContent = document.querySelector('#advice-list-content');
+const ListContainer = document.querySelector('#advice-list-container');
 const submitBtn = document.querySelector("#submit-btn");
 const removeBtn = document.querySelector('#remove-btn');
 const url = `https://api.adviceslip.com/advice`;
@@ -7,6 +8,8 @@ let previousAdvice;
 
 submitBtn.addEventListener("click", displayAdvice);
 removeBtn.addEventListener("click", removeListOfAdvice);
+hideListContainer();
+
 
 /*This function fetches the data from API and display it in the DOM.*/
 function displayAdvice() {   
@@ -15,9 +18,13 @@ function displayAdvice() {
             return response.json();
         }).then(
         (adviceData) => {
-            console.log(adviceData)
             const advice = adviceData.slip.advice;
-            currentAdvice.innerHTML = advice;
+            if (advice===previousAdvice) { //I added this as I have noticed that the data being fetched is repeated.
+                displayAdvice()
+                return
+            }
+            console.log(advice)
+            currentAdvice.innerHTML ='"'+ advice + '"';
             if (previousAdvice) {
                 addAdviceToList(previousAdvice);
             }
@@ -34,9 +41,19 @@ function addAdviceToList(advice) {
     const adviceListItem = document.createElement('li');
     adviceListItem.textContent = advice;
     adviceListContent.appendChild(adviceListItem);
-
+    showListContainer();
 }
 /*This function clears the list in the DOM*/
 function removeListOfAdvice() {
     adviceListContent.textContent = '';
+    removeBtn.style.visibility = 'hidden';
 }
+
+/*Functions to show and hide the list container in the DOM */
+function showListContainer () {
+    ListContainer.style.visibility = '' ;
+}
+
+function hideListContainer () {
+    ListContainer.style.visibility = 'hidden';
+} 
